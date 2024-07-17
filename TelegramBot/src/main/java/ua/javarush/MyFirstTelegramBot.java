96b5c0a147782bc397e7725cdff04917ce30b6e6
shemchuk <shemchukaga@gmail.com>
2024-07-17 08:36:57 +0200

    add file wasya

--- TelegramBot/src/main/java/ua/javarush/MyFirstTelegramBot.java
+++ TelegramBot/src/main/java/ua/javarush/MyFirstTelegramBot.java
@@ -29,29 +29,81 @@ public class MyFirstTelegramBot extends TelegramLongPollingBot {
         Long chatId = getChatId(update);
 
         if (update.hasMessage() && update.getMessage().getText().equals("/start")) {
-            SendMessage message = createMessage(chatId, "Привіт *Ґамлєт*, _ходімо_ в кабінєт!");
+            addGlories(chatId, 0);
+            SendMessage message = createMessage(chatId, STEP_1_TEXT, Map.of(
+                    "Злам холодильника", "step_1_btn"
+            ));
             sendApiMethodAsync(message);
         }
-
-        if (update.hasMessage() && update.getMessage().getText().toLowerCase().contains("привіт")) {
-            SendMessage message = createMessage(chatId, "Ти хто? Як звать тебе? То може ти ще й *сала* не їси?");
-            sendApiMethodAsync(message);
-        }
-
-        if (update.hasMessage() &&
-                (update.getMessage().getText().toLowerCase().contains("їм") ||
-                        update.getMessage().getText().toLowerCase().contains("їдю") ||
-                        update.getMessage().getText().toLowerCase().contains("обожнюю") ||
-                        update.getMessage().getText().toLowerCase().contains("люблю") ||
-                        update.getMessage().getText().toLowerCase().contains("тащусь"))) {
-            SendMessage message = createMessage(chatId, "Я знав, ти _друг_, я ж *кіт*.");
-            sendApiMethodAsync(message);
-            return;
-        }
-
-        if (update.hasMessage()) {
-            SendMessage message = createMessage(chatId, "Забирай свої тракторці та іди з мого пісочку, _непевний_ *типе*!");
-            sendApiMethodAsync(message);
+        if (update.hasCallbackQuery()) {
+            if (update.getCallbackQuery().getData().equals("step_1_btn") && getGlories(chatId) == 0) {
+                addGlories(chatId, 20);
+                SendMessage message = createMessage(chatId, STEP_2_TEXT,
+                        Map.of("Взяти сосиску! +20 слави", "step_2_btn",
+                                "Взяти рибку! +20 слави", "step_2_btn",
+                                "Скинути банку з огірками! +20 слави", "step_2_btn"));
+                sendApiMethodAsync(message);
+            }
+            if (update.getCallbackQuery().getData().equals("step_2_btn") && getGlories(chatId) == 20) {
+                addGlories(chatId, 20);
+                SendMessage message = createMessage(chatId, STEP_3_TEXT,
+                        Map.of(
+                                "Злам робота пилососа", "step_3_btn"
+                        ));
+                sendApiMethodAsync(message);
+            }
+            if (update.getCallbackQuery().getData().equals("step_3_btn") && getGlories(chatId) == 40) {
+                addGlories(chatId, 30);
+                SendMessage message = createMessage(chatId, STEP_4_TEXT,
+                        Map.of(
+                                "Відправити робопилосос за їжею! +30 слави", "step_4_btn",
+                                "Проїхатися на робопилососі! +30 слави", "step_4_btn",
+                                "Тікати від робопилососа! +30 слави", "step_4_btn"
+                        ));
+                sendApiMethodAsync(message);
+            }
+            if (update.getCallbackQuery().getData().equals("step_4_btn") && getGlories(chatId) == 70) {
+                addGlories(chatId, 30);
+                SendMessage message = createMessage(chatId, STEP_5_TEXT,
+                        Map.of(
+                                "Одягнути та включити GoPro!", "step_5_btn"
+                        ));
+                sendApiMethodAsync(message);
+            }
+            if (update.getCallbackQuery().getData().equals("step_5_btn") && getGlories(chatId) == 100) {
+                addGlories(chatId, 40);
+                SendMessage message = createMessage(chatId, STEP_6_TEXT,
+                        Map.of(
+                                "Одягнути та включити GoPro!", "step_6_btn"
+                        ));
+                sendApiMethodAsync(message);
+            }
+            if (update.getCallbackQuery().getData().equals("step_6_btn") && getGlories(chatId) == 140) {
+                addGlories(chatId, 40);
+                SendMessage message = createMessage(chatId, STEP_7_TEXT,
+                        Map.of(
+                                "Бігати дахами, знімати на GoPro! +40 слави", "step_7_btn",
+                                "GoPro нападати на інших котів із засідки! +40 слави", "step_7_btn",
+                                "GoPro нападати на собак із засідки! +40 слави", "step_7_btn"
+                        ));
+                sendApiMethodAsync(message);
+            }
+            if (update.getCallbackQuery().getData().equals("step_7_btn") && getGlories(chatId) == 180) {
+                addGlories(chatId, 50);
+                SendMessage message = createMessage(chatId, STEP_8_TEXT,
+                        Map.of(
+                                "Злам пароля", "step_8_btn"
+                        ));
+                sendApiMethodAsync(message);
+            }
+            if (update.getCallbackQuery().getData().equals("step_8_btn") && getGlories(chatId) == 230) {
+                addGlories(chatId, 0);
+                SendMessage message = createMessage(chatId, FINAL_TEXT ,
+                        Map.of(
+                                "Вийти на подвір'я", "step_final_btn"
+                        ));
+                sendApiMethodAsync(message);
+            }
         }
     }
 
